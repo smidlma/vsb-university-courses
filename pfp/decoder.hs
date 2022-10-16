fce118391759446cac12a2438aa16f1534a1b89d
Martin Šmídl <smidl.martin1@gmail.com>
2022-10-16 11:11:51 +0200

    haskell simple decoder

--- pfp/decoder.hs
+++ pfp/decoder.hs
@@ -1,41 +1,17 @@
 -- DU 4 homework 1 FPR
-type Result = [String]
+findEncoded :: Char -> [(Char, String)] -> [String]
+findEncoded c voc = map (\(key, value) -> value) (filter (\(key, value) -> key == c) voc)
 
-pp :: Result -> IO ()
-pp x = putStr (concat (map (++ "\n") x))
+repl :: Char -> [(Char, String)] -> [Char]
+repl c voc =
+  let tmp = findEncoded c voc
+   in if null tmp then [c] else head tmp
 
--- replace' str voc = map str
--- replace' c voc
---   | isReplacable c voc = 'a'
---   | otherwise = c
+fromEncode :: [Char] -> [(Char, String)] -> [[Char]]
+fromEncode str voc = map (\x -> repl x voc) str
 
--- isReplacable c voc = filter (\(key, value) -> key == c) voc
-
-findInVoc c voc = map (\(key, value) -> head value) (filter (\(key, value) -> key == c) voc)
-
-replace' _ _ [] = []
-replace' c val (x : xs)
-  | c == x = val ++ replace' c val xs
-  | otherwise = x : replace' c val xs
-
-mapValues xs voc = [(findInVoc x voc) | x <- xs]
-
-test' xs = [x | x <- xs, x == 'A']
-
--- replace' xs voc =
--- replace' c voc = c
-
--- splitCode str = [[x] | x <- str]
-
--- decode hash vocabulary = [isReplacable x vocabulary | x <- hash]
-
--- decode :: String -> [(Char, String)] -> String
 -- decode "HAHA" [('E',"AB"),('F',"CD"),('G',"EF"),('H',"GG")]
--- HAHA
--- GGAHA
--- EFGAHA
--- ABFGAHA
--- ABCDGAHA
--- "ABCDABCDAABCDABCDA"
--- 1. check first char against vocabulary
--- 2. if find letter from voc take start of code insert
+decode :: String -> [(Char, String)] -> String
+decode str voc =
+  let tmp = foldl (++) [] (fromEncode str voc)
+   in if tmp == str then tmp else decode tmp voc
